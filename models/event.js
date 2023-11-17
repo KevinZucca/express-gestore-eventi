aaec756b078eee09624604fd88185ad066aaba19
KevinZucca <kev.94@live.it>
2023-11-17 17:17:43 +0100

    feat: getter and setter of classes

--- models/event.js
+++ models/event.js
@@ -2,12 +2,70 @@ const fs = require("fs");
 const path = require("path");
 
 class Model {
+  #id;
+  #title;
+  #description;
+  #date;
+  #maxSeats;
+
   constructor(id, title, description, date, maxSeats) {
-    (this.id = id),
-      (this.title = title),
-      (this.description = description),
-      (this.date = date),
-      (this.maxSeats = maxSeats);
+    this.id = id;
+    this.title = title;
+    this.description = description;
+    this.date = date;
+    this.maxSeats = maxSeats;
+  }
+
+  get title() {
+    return this.#title;
+  }
+
+  get description() {
+    return this.#description;
+  }
+
+  get date() {
+    return this.#date;
+  }
+
+  get maxSeats() {
+    return this.#maxSeats;
+  }
+
+  set title(value) {
+    if (typeof value != "string") {
+      throw new Error("Il titolo deve essere una stringa");
+    }
+    if (value.length > 15) {
+      throw new Error("Il titolo deve avere meno di 15 caratteri ");
+    }
+    this.#title = value;
+  }
+
+  set description(value) {
+    if (typeof value != "string") {
+      throw new Error("La descrizione deve essere una stringa");
+    }
+    if (value.length > 50) {
+      throw new Error("La descrizione deve avere meno di 50 caratteri ");
+    }
+    this.#description = value;
+  }
+
+  set date(value) {
+    if (typeof value != "string") {
+      throw new Error("La data deve essere in formato stringa");
+    }
+    this.#date = value;
+  }
+
+  set maxSeats(value) {
+    if (typeof value != "number") {
+      throw new Error(
+        "Il numero massimo di posti deve essere definito come numero"
+      );
+    }
+    this.#maxSeats = value;
   }
 
   static seeEvents(query) {
--- models/reservation.js
+++ models/reservation.js
@@ -1,10 +1,62 @@
 class Reservation {
+  #id;
+  #firstName;
+  #lastName;
+  #email;
+  #eventId;
   constructor(id, firstName, lastName, email, eventId) {
-    (this.id = id),
-      (this.firstName = firstName),
-      (this.lastName = lastName),
-      (this.email = email),
-      (this.eventId = eventId);
+    this.id = id;
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.email = email;
+    this.eventId = eventId;
+  }
+
+  get firstName() {
+    return this.#firstName;
+  }
+  get lastName() {
+    return this.#lastName;
+  }
+  get email() {
+    return this.#email;
+  }
+  get eventId() {
+    return this.#eventId;
+  }
+
+  set firstName(value) {
+    if (typeof value != "string") {
+      throw new Error("Il nome deve essere una stringa");
+    }
+    if (value.length > 15) {
+      throw new Error("Il nome non può avere più di 15 caratteri ");
+    }
+    this.#firstName = value;
+  }
+
+  set lastName(value) {
+    if (typeof value != "string") {
+      throw new Error("Il cognome deve essere una stringa");
+    }
+    if (value.length > 15) {
+      throw new Error("Il cognome non può avere più di 15 caratteri ");
+    }
+    this.#lastName = value;
+  }
+
+  set email(value) {
+    if (typeof value != "string") {
+      throw new Error("L'email deve essere una stringa");
+    }
+    this.#email = value;
+  }
+
+  set eventId(value) {
+    if (typeof value != "number") {
+      throw new Error("L'id dell'evento deve essere un numero");
+    }
+    this.#eventId = value;
   }
 }
 
