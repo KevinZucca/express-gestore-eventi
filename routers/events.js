b08e6519bedb752373cf35aedcc7a9f4a618c6bb
KevinZucca <kev.94@live.it>
2023-11-17 10:29:07 +0100

    feat: routers and controller first scaffolding

---
+++ routers/events.js
@@ -0,0 +1,5 @@
+// const express = require("express");
+// const router = express.Router();
+// const eventsController = require("../controllers/events");
+
+// router.get("/", eventsController.index);
--- server.js
+++ server.js
@@ -3,6 +3,11 @@ const app = express();
 const dotenv = require("dotenv").config();
 const port = process.env.PORT;
 
+// routes
+app.get("/events", eventsRouter);
+app.post("/events", eventsRouter);
+app.put("/event/:event", eventsRouter);
+
 app.listen(port || 8000, () => {
   console.log(`server is running on port ${port}`);
 });
